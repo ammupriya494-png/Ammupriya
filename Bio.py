7b13cd1974988a8664822cf89e12180ae53e7700
ammupriya494-png <ammupriya494@gmail.com>
2025-11-05 11:51:24 +0530

    Add files via upload

---
+++ Bio.py
@@ -0,0 +1,40 @@
+print("Hello world");
+
+print("What is your first name?")
+first_name=input()
+print("What is your last name?")
+last_name=input()
+print("Hey" + first_name + "" + last_name)
+
+
+#Integer
+integer_1=10
+integer_2=0
+
+# Float
+float_1=12.3
+float_2=5.2
+
+#String
+string_1="this is a sentence"
+string_2="and this is another"
+
+# Boolean
+boolean_1=True
+boolean_2=False
+
+# Lists
+list_1=(
+    "bacon",
+    "eggs",
+    "cheese"
+    )
+list_2=(
+    "banana",
+    "pasta",
+    "kiwi",
+    "Mango"
+    )
+
+
+
