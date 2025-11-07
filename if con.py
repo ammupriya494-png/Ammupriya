b6ca20535e0f14cd9c80a4f2815dd85af8c59da3
ammupriya494-png <ammupriya494@gmail.com>
2025-11-07 17:24:06 +0530

    Add files via upload

---
+++ if con.py
@@ -0,0 +1,7 @@
+food = input("What is the best food?:")
+if food == "rice":
+    print("yes, rice is the best.")
+elif food == "apple":
+    print("Apples aren't my cup of rice.")
+else:
+    print("Never heard of it")
